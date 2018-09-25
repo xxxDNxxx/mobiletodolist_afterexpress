edaa274c9094a90c0b6b39fa47960267c9329285
xxxDNxxx <beerurban@gmail.com>
2018-09-25 16:20:42 +0700

    complete59160379

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const todo = require('./todo.js');
-var port = process.env.PORT || 3000; //
+var port = process.env.PORT || 3233; //พอร์ทในการรัน
 var cors = require('cors')
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(cors())                                  //**
+app.use(bodyParser.urlencoded({extended:true})); //สร้างเว็บเซิร์ฟเวอร์
+app.use(bodyParser.json());                      //**
+app.use('/todo',todo); //เรียกapiมาหน้าแรก
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- todo.js
+++ todo.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 const f = require('util').format;
 
-const user = encodeURIComponent(''); // 
-const password = encodeURIComponent(''); //
-var dbName = "" // 
+const user = encodeURIComponent('admin'); // username ใน database mongo ที่สร้าง
+const password = encodeURIComponent('admin123456'); //
+var dbName = "mobileparadigm" //
 
 var MongoClient = require('mongodb').MongoClient;
-// moogose 
-const dbUrl = f("mongodb://%s:%s@ds245512.mlab.com:45512/%s", user, password, dbName);
+// moogose
+const dbUrl = f("mongodb://%s:%s@ds111913.mlab.com:11913/%s", user, password, dbName);//%sคือการbinding
 
 var ObjectID = require('mongodb').ObjectID;
 
-router.get('/', function (req, res) {
+router.get('/', function (req, res) { //เรียกการใช้ข้อมูลแบบ get
     MongoClient.connect(dbUrl, function(err, client) {
 		const db = client.db(dbName).collection('todo').find({}).toArray(function (err, result) {
 			client.close();
@@ -25,11 +25,19 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-	
-})
+  MongoClient.connect(dbUrl, function(err, client) {
+  const db = client.db(dbName).collection('todo').InsertOne(req.body,function (err, result) {
+    client.close();
+    if (err) res.send(err);
+    res.status(200);
+    res.send("insert success");
+  });
+  });
+  });
+
 
 router.delete('/', function (req, res) {
-		
+
 })
 
 router.put('/', function (req, res) {
